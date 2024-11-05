8f2d8c1e7abb8caa06ff791aeac38cfd90f7b0df
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-11-05 17:24:00 -0300

    feat: add validators to the entity

--- jest.config.ts
+++ jest.config.ts
@@ -176,8 +176,7 @@ const config: Config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    // "^.+\\.(t|j)sx?$": "@swc/jest",
-    "^.+.(t|j)sx?$": ["ts-jest", {}],
+    "^.+\\.(t|j)sx?$": "@swc/jest",
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
