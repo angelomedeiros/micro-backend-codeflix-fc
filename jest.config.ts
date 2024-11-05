29b2fa2612be3534f5ade8ef13103007ffad4967
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-11-05 19:15:31 -0300

    chore: fix path in jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -137,7 +137,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["./infra/testing/expect_helpers.ts"],
+  setupFilesAfterEnv: ["./shared/infra/testing/expect_helpers.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
